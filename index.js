b28d61e3624b2296c9a3f461dca399660754440b
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2023-09-27 07:51:14 -0700

    this is the final version of the software until i record the video for the challenge

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ inquirer.prompt ([
     type: 'list',
     name: 'license',
     message: 'What license would you like your README to contain?',
-    choices: ["MIT","GPLv2","Apache","GPLv3","No license"],
+    choices: ["MIT","Apache","GPLv3",],
     },
     
     {        
@@ -88,13 +88,22 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License.`
+limitations under the License.` ,
+
+"GPLv3":`
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.   
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0-standalone.html>.`
     }
+    
+
 
 
 
 //! Because im using template literal it has to be written like this or it indents itself on the readme
-    var readMeFile = `# ${response.title} ![License: ${response.license}](https://img.shields.io/badge/License-${response.license}-green.svg)
+    var readMeFile = `# ${response.title} ![License: ${response.license}](https://img.shields.io/badge/License-${response.license}-blue.svg)
 ## Description <br>
 ${response.description} <br>
 ## Table of contents <br>
